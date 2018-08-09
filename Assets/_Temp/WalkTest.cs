f5d3cda2eea20abfd8321cd4b4111b346de6f2e7
PuddingPudding <hwsh70119@gmail.com>
2018-08-09 22:33:34 +0800

    練習blend tree

---
+++ Assets/_Temp/WalkTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkTest : MonoBehaviour
+{
+
+    private Animator m_anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.m_anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.m_anim.SetBool("Walking", true);
+            this.m_anim.SetFloat("TiredValue", 1);
+        }
+        else
+        {
+            this.m_anim.SetBool("Walking", false);
+            this.m_anim.SetFloat("TiredValue", 0);
+        }
+    }
+}
