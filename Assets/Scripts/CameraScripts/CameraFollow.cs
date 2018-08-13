3b307f00d36d835c5a682e76aa8a04dd81cc1c61
PuddingPudding <hwsh70119@gmail.com>
2018-08-13 23:54:12 +0800

    新增camera追蹤玩家功能，使用lerp至做出平順的追蹤效果，另外也試著改變了游標在遊戲中的外觀

---
+++ Assets/Scripts/CameraScripts/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private float m_fFollowHeight = 8f; //跟蹤高度
+    [SerializeField] private float m_fFollowDistance = 6f; //跟蹤距離
+    [SerializeField] private Transform m_player;
+
+    private float m_fTargetHeight;
+    private float m_fCurrentHeight;
+    private float m_fCurrentRotation;
+
+    // Use this for initialization
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.m_fTargetHeight = this.m_player.position.y + this.m_fFollowHeight; //最終要達到的高度
+        this.m_fCurrentRotation = this.transform.eulerAngles.y;
+        this.m_fCurrentHeight = Mathf.Lerp(this.transform.position.y, this.m_fTargetHeight, 0.9f * Time.deltaTime);
+        //Lerp會去計算出參數1向參數2邁進多少%的值，例如今天後面填1的話就會直接回傳參數2，填0.5f的話就會回傳兩者相加的平均
+        //this.m_fCurrentHeight = this.m_fTargetHeight; //上面版本能讓畫面上下浮動時變得平順
+        Quaternion euler = Quaternion.Euler(0, this.m_fCurrentRotation, 0);
+        Vector3 targetPos = this.m_player.position - (euler * Vector3.forward) * this.m_fFollowDistance;
+        targetPos.y = this.m_fCurrentHeight;
+
+        this.transform.position = targetPos;
+        this.transform.LookAt(this.m_player);
+    }
+}
---
+++ Assets/Scripts/CameraScripts/MouseScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseScript : MonoBehaviour
+{
+    [SerializeField] Texture2D m_cursorTexture;
+    //[SerializeField] GameObject m_mousePoint;
+    private CursorMode m_mode = CursorMode.ForceSoftware;
+    //要求游標按照我們所寫的去變話，不要跟著當下所運行之平台
+    private Vector2 m_v2HotSpot = Vector2.zero;
+
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.SetCursor(this.m_cursorTexture, this.m_v2HotSpot, this.m_mode);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
