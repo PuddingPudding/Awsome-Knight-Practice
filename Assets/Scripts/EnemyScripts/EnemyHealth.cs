1a5bc2f973c74b7d5af1af587251684c84e5e5e9
PuddingPudding <hwsh70119@gmail.com>
2018-09-13 00:05:21 +0800

    新增讓敵人受傷的判定

---
+++ Assets/Scripts/EnemyScripts/EnemyHealth.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+    [SerializeField] private float m_fHealth = 100f;
+
+    public void TakeDmg(float _fDmg)
+    {
+        m_fHealth -= _fDmg;
+        print("敵人承受" + _fDmg + "傷害，還剩" + m_fHealth + "HP");
+        if(m_fHealth <= 0)
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/FXScripts/SkillDamage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillDamage : MonoBehaviour
+{
+
+    [SerializeField] private LayerMask m_enemyLayer;
+    [SerializeField] private float m_fRadius = 0.5f;
+    [SerializeField] private float m_fDmgCount = 10;
+
+    private EnemyHealth m_enemyHealth;
+    private bool m_bColided = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, m_fRadius, m_enemyLayer);
+
+        foreach(Collider c in hits)
+        {
+            if(c.isTrigger)
+            {
+                continue;
+            }
+            m_enemyHealth = c.gameObject.GetComponent<EnemyHealth>();
+            m_bColided = true;
+        }
+        if(this.m_bColided)
+        {
+            m_enemyHealth.TakeDmg(m_fDmgCount);
+            this.enabled = false;
+        }
+    }
+}
